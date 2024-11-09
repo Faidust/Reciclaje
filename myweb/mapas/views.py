9a631bc58d21ffe854850723ba84fb03abee194d
Faidust <tbgp1990@gmail.com>
2024-11-09 00:14:51 -0300

    progreso en los mapas

--- myweb/mapas/views.py
+++ myweb/mapas/views.py
@@ -29,3 +29,81 @@ def carton(request):
     folium.CircleMarker([max_lat, max_lon], tooltip="Upper Right Corner").add_to(initialmap)
     context={"mapas":initialmap._repr_html_()}
     return render(request,'carton.html',context)
+
+def latas(request):
+    texto_carton="<h1>Patio principal</h1>"
+    min_lon, max_lon = -70.62020630592322, -70.61816934867323
+    min_lat, max_lat = -33.491713968794016, -33.489445772654996
+    initialmap= folium.Map(max_bounds=True, location=[-33.49061291008033, -70.61921242455158], zoom_start=18,min_lat=min_lat, max_lat=max_lat, min_lon=min_lon, max_lon=max_lon)
+    folium.Marker(location=[-33.490106475860095, -70.61868275699759],popup=texto_carton).add_to(initialmap)
+    folium.CircleMarker([max_lat, min_lon], tooltip="Upper Left Corner").add_to(initialmap)
+    folium.CircleMarker([min_lat, min_lon], tooltip="Lower Left Corner").add_to(initialmap)
+    folium.CircleMarker([min_lat, max_lon], tooltip="Lower Right Corner").add_to(initialmap)
+    folium.CircleMarker([max_lat, max_lon], tooltip="Upper Right Corner").add_to(initialmap)
+    context={"mapas":initialmap._repr_html_()}
+    return render(request,'latas.html',context)
+
+def vidrio(request):
+    texto_carton="<h1>Patio principal</h1>"
+    min_lon, max_lon = -70.62020630592322, -70.61816934867323
+    min_lat, max_lat = -33.491713968794016, -33.489445772654996
+    initialmap= folium.Map(max_bounds=True, location=[-33.49061291008033, -70.61921242455158], zoom_start=18,min_lat=min_lat, max_lat=max_lat, min_lon=min_lon, max_lon=max_lon)
+    folium.Marker(location=[-33.490106475860095, -70.61868275699759],popup=texto_carton).add_to(initialmap)
+    folium.CircleMarker([max_lat, min_lon], tooltip="Upper Left Corner").add_to(initialmap)
+    folium.CircleMarker([min_lat, min_lon], tooltip="Lower Left Corner").add_to(initialmap)
+    folium.CircleMarker([min_lat, max_lon], tooltip="Lower Right Corner").add_to(initialmap)
+    folium.CircleMarker([max_lat, max_lon], tooltip="Upper Right Corner").add_to(initialmap)
+    context={"mapas":initialmap._repr_html_()}
+    return render(request,'vidrio.html',context)
+
+def papel(request):
+    texto_carton="<h1>Patio principal</h1>"
+    min_lon, max_lon = -70.62020630592322, -70.61816934867323
+    min_lat, max_lat = -33.491713968794016, -33.489445772654996
+    initialmap= folium.Map(max_bounds=True, location=[-33.49061291008033, -70.61921242455158], zoom_start=18,min_lat=min_lat, max_lat=max_lat, min_lon=min_lon, max_lon=max_lon)
+    folium.Marker(location=[-33.490106475860095, -70.61868275699759],popup=texto_carton).add_to(initialmap)
+    folium.CircleMarker([max_lat, min_lon], tooltip="Upper Left Corner").add_to(initialmap)
+    folium.CircleMarker([min_lat, min_lon], tooltip="Lower Left Corner").add_to(initialmap)
+    folium.CircleMarker([min_lat, max_lon], tooltip="Lower Right Corner").add_to(initialmap)
+    folium.CircleMarker([max_lat, max_lon], tooltip="Upper Right Corner").add_to(initialmap)
+    context={"mapas":initialmap._repr_html_()}
+    return render(request,'papel.html',context)
+
+def latasal(request):
+    texto_carton="<h1>Patio principal</h1>"
+    min_lon, max_lon = -70.62020630592322, -70.61816934867323
+    min_lat, max_lat = -33.491713968794016, -33.489445772654996
+    initialmap= folium.Map(max_bounds=True, location=[-33.49061291008033, -70.61921242455158], zoom_start=18,min_lat=min_lat, max_lat=max_lat, min_lon=min_lon, max_lon=max_lon)
+    folium.Marker(location=[-33.490106475860095, -70.61868275699759],popup=texto_carton).add_to(initialmap)
+    folium.CircleMarker([max_lat, min_lon], tooltip="Upper Left Corner").add_to(initialmap)
+    folium.CircleMarker([min_lat, min_lon], tooltip="Lower Left Corner").add_to(initialmap)
+    folium.CircleMarker([min_lat, max_lon], tooltip="Lower Right Corner").add_to(initialmap)
+    folium.CircleMarker([max_lat, max_lon], tooltip="Upper Right Corner").add_to(initialmap)
+    context={"mapas":initialmap._repr_html_()}
+    return render(request,'latas_al.html',context)
+
+def tapas(request):
+    texto_carton="<h1>Patio principal</h1>"
+    min_lon, max_lon = -70.62020630592322, -70.61816934867323
+    min_lat, max_lat = -33.491713968794016, -33.489445772654996
+    initialmap= folium.Map(max_bounds=True, location=[-33.49061291008033, -70.61921242455158], zoom_start=18,min_lat=min_lat, max_lat=max_lat, min_lon=min_lon, max_lon=max_lon)
+    folium.Marker(location=[-33.490106475860095, -70.61868275699759],popup=texto_carton).add_to(initialmap)
+    folium.CircleMarker([max_lat, min_lon], tooltip="Upper Left Corner").add_to(initialmap)
+    folium.CircleMarker([min_lat, min_lon], tooltip="Lower Left Corner").add_to(initialmap)
+    folium.CircleMarker([min_lat, max_lon], tooltip="Lower Right Corner").add_to(initialmap)
+    folium.CircleMarker([max_lat, max_lon], tooltip="Upper Right Corner").add_to(initialmap)
+    context={"mapas":initialmap._repr_html_()}
+    return render(request,'tapas.html',context)
+
+def electro(request):
+    texto_carton="<h1>Patio principal</h1>"
+    min_lon, max_lon = -70.62020630592322, -70.61816934867323
+    min_lat, max_lat = -33.491713968794016, -33.489445772654996
+    initialmap= folium.Map(max_bounds=True, location=[-33.49061291008033, -70.61921242455158], zoom_start=18,min_lat=min_lat, max_lat=max_lat, min_lon=min_lon, max_lon=max_lon)
+    folium.Marker(location=[-33.490106475860095, -70.61868275699759],popup=texto_carton).add_to(initialmap)
+    folium.CircleMarker([max_lat, min_lon], tooltip="Upper Left Corner").add_to(initialmap)
+    folium.CircleMarker([min_lat, min_lon], tooltip="Lower Left Corner").add_to(initialmap)
+    folium.CircleMarker([min_lat, max_lon], tooltip="Lower Right Corner").add_to(initialmap)
+    folium.CircleMarker([max_lat, max_lon], tooltip="Upper Right Corner").add_to(initialmap)
+    context={"mapas":initialmap._repr_html_()}
+    return render(request,'electro.html',context)
