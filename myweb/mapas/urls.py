0e9a3cdfd51c400dfad78153be6a8a0f28663120
Faidust <tbgp1990@gmail.com>
2024-11-13 03:44:56 -0300

    termino de los mapas

--- myweb/mapas/urls.py
+++ myweb/mapas/urls.py
@@ -8,7 +8,6 @@ urlpatterns=[
     path("latas/", views.latas, name="latas"),
     path("vidrio/", views.vidrio, name="vidrio"),
     path("papel/", views.papel, name="papel"),
-    path("latas aluminio/", views.latasal, name="latasal"),
     path("tapas/", views.tapas, name="tapas"),
     path("electro/", views.electro, name="electro"),
 ]
