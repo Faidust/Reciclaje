49c10e41236ddacbe9ed8069836ea6dad878dc62
dianidianita <gutierrez.rdc@gmail.com>
2024-11-29 15:45:45 -0300

    nivel listo falta diseño

--- myweb/nivel/views.py
+++ myweb/nivel/views.py
@@ -2,58 +2,53 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
 from .models import Palabra, Reciclaje
+from django.db.models import Sum
 
 
 @login_required
 def nivel(request):
-    return render(request, "nivel.html")
+    total_palabras = Palabra.objects.filter(usuario=request.user).aggregate(Sum('cantidad'))['cantidad__sum'] or 0
+    return render(request, "nivel.html", {'total_palabras': total_palabras})
+
 
 @login_required
 def guardar_palabra(request):
     if request.method == 'POST':
-        cantidad = 0
         palabra = request.POST['palabra']
         usuario = request.user
         Palabra_obj, created = Palabra.objects.get_or_create(usuario=usuario, Palabra=palabra)
         Palabra_obj.cantidad += 1
         Palabra_obj.save()
-        numnivel = 0
-        nombrenivel = ""
-        palabras = Palabra.objects.filter(usuario=request.user)
-        total_palabras = palabras.order_by('-id').first().id if palabras.exists() else None
         
-        if total_palabras >= 5:  # Por ejemplo, cada 10 palabras
-            numnivel += 1
-            nombrenivel = "Iniciador de Cambios"
-
-        elif total_palabras >= 10:
-            numnivel += 1
-            nombrenivel = "Aprendiz"
-        elif total_palabras >= 15:
-            numnivel += 1
-            nombrenivel = "Maestro"
-        else:
-            numnivel +=1
-            nombrenivel = "Guerrero comprometido"
-            
     return redirect('usuariodos')
-    return render(request, 'nivel.html',{
-        'nombrenivel' : nombrenivel
-    })
+    
 
 @login_required
 def usuariodos(request):
-   
+    usuario = request.user
     palabras = Palabra.objects.filter(usuario=request.user)
-    total_palabras = palabras.order_by('-id').first().id if palabras.exists() else None
-    '''return render(request, 'nivel.html', {'palabras': palabras, 'total_palabras': total_palabras})'''
     ultimas_palabras = palabras.order_by('-id')[:3]
-    
+    total_palabras = Palabra.objects.filter(usuario=request.user).aggregate(Sum('cantidad'))['cantidad__sum'] or 0
+    numnivel = 0
+    if total_palabras <= 5:  # Por ejemplo, cada 10 palabras
+        numnivel += 1
+        nombrenivel = "Iniciador de Cambios"
+
+    elif total_palabras <= 10:
+        numnivel += 2
+        nombrenivel = "Aprendiz"
+    elif total_palabras <= 15:
+        numnivel += 3
+        nombrenivel = "Maestro"
+    else:
+        numnivel +=4
+        nombrenivel = "Guerrero comprometido"
     return render(request, 'nivel.html', {
-        #'usuario': usuario,
         'palabras': palabras,
-        'total_palabras': total_palabras,
         'ultimas_palabras': ultimas_palabras,
-        
+        'usuario': usuario,
+        'total_palabras': total_palabras,
+        'nombrenivel': nombrenivel,
+        'numnivel': numnivel
         
     })
